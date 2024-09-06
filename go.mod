10be960c6ad11c54c8f6209a0b0f6c5b15054faf
pulkitsh1 <pulkitsh1@outlook.com>
2024-09-06 11:34:52 +0530

    added Broadcast

---
+++ go.mod
@@ -0,0 +1,5 @@
+module chat-feed
+
+go 1.21.1
+
+require golang.org/x/net v0.28.0 // indirect
---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
+	"golang.org/x/net/websocket"
+)
+
+type Server struct {
+	conns map[*websocket.Conn]bool
+}
+
+func NewServer() *Server {
+	return &Server{
+		conns: make(map[*websocket.Conn]bool),
+	}
+}
+
+func (s *Server) handleWSOrderbooks(ws *websocket.Conn) {
+	fmt.Println("New incoming connection from client to orderbook feed: ", ws.RemoteAddr())
+
+	for {
+		payload := fmt.Sprintf("orderbook data -> %d\n", time.Now().UnixNano())
+		ws.Write([]byte(payload))
+		time.Sleep(time.Second * 2)
+	}
+}
+
+func (s *Server) handleWS(ws *websocket.Conn) {
+	fmt.Println("new incoming connection from client: ", ws.RemoteAddr())
+
+	s.conns[ws] = true
+	s.readLoop(ws)
+}
+
+func (s *Server) readLoop(ws *websocket.Conn) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := ws.Read(buf)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("read error: ", err)
+			continue
+		}
+		msg := buf[:n]
+		fmt.Printf("%s\n", msg)
+
+		ws.Write([]byte("Thank you for the message!!!"))
+
+		s.Broadcast(msg)
+	}
+}
+
+func (s *Server) Broadcast(b []byte) {
+	for ws := range s.conns {
+		go func(ws *websocket.Conn) {
+			if _, err := ws.Write(b); err != nil {
+				fmt.Println("write error: ", err)
+			}
+		}(ws)
+	}
+}
+
+func main() {
+	server := NewServer()
+	http.Handle("/ws", websocket.Handler(server.handleWS))
+	http.Handle("/handleorderbooks", websocket.Handler(server.handleWSOrderbooks))
+	http.ListenAndServe(":3000", nil)
+}
